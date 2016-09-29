8f8c511cda2d4beced1f4c4e849fd95223a723e0
Christie Bahlai <cbahlai@msu.edu>
2016-09-29 15:46:47 -0400

    create data pre-processing scripts

---
+++ regime_shift_detector.R
@@ -0,0 +1,35 @@
+#script to analyse time series data to etermine break points
+
+#import test data for calibrating functions
+test<-read.csv(file="test.csv", header=TRUE)
+
+
+#assume data is in form of data frame where first column is year, second is abundance (Nt)
+
+#create function that makes an additional response variable Nt1
+#where Nt1 is the abundance in the next year
+
+addNt1<-function(data){
+  #create empty vector to feed 'next year' abundance into
+  Nt1 = c()
+  #pull out values from the abundance column-
+  #for each year, go to the NEXT year's abundance, put that in the vector
+  for (i in 1:(length(data[,2])-1)) {
+    Nt1 = c(Nt1,(data[,2])[i+1])
+  }
+  #cut out last sampling year, because there is no Nt+1 for that year
+  data<-data[which(data[,1]<max(data[,1])),]
+  #add the vector of new values to the data fram
+  data$Nt1<-Nt1
+  #spit out that new data frame
+  return(data)
+}
+
+#test that our function does what we want it to do
+test1<-addNt1(test)
+view(test1)
+
+#looks good! now we can begin building the model-fitting function
+
+
+#create function that fits the ricker model
