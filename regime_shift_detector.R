a0443b6a65e95dd9a6282a93f386a222b8f7389e
Christie Bahlai <cbahlai@msu.edu>
2016-11-17 21:25:08 -0500

    build a test for linear decline in k

--- regime_shift_detector.R
+++ regime_shift_detector.R
@@ -315,7 +315,7 @@ test2<-test1
 test2$r<-r.est(test2)
 
 k.est<-function(data){
-  r<-rickerfit(data)[2]# fit model without breakpints to get the estimated overall k
+  r<-rickerfit(data)[2]# fit model without breakpints to get the estimated overall r
   k<- data$Nt/(1-(log(data$Nt1/data$Nt)/r)) #solve for k
   return (k)
 }
@@ -346,11 +346,13 @@ monarch1$k<-k.est(monarch1)
 #so what I'm seeing in these data is it looks like the assumtion that k is constant 
 #is a really bad one. the regime shift model seems to suggest that r is even going up as 
 # k drops dramatically. Not to speculate or anything, but this is what we'd expect in
-# a case where habitat destruction is limiting breeding habitat, but that in turn is reducing 
-#competition, so individuals are putting more offspring out there. 
+# a case where habitat destruction is limiting breeding habitat, but there could be something 
+#compensatory going on with r.
 
-#that's interesting. So we haven't considered a case where there's a linear decline in k. 
-# we asumed brupt transitions, but this may not be the case. Let's see what happens when 
+#that's interesting. 
+
+#So we haven't considered a case where there's a linear decline in k. 
+# we asumed abrupt transitions, but this may not be the case. Let's see what happens when 
 #we look at the data this way. 
 
 # a good guess for the form of a linear decline in k, if it's occuring, would be a simple 
@@ -366,7 +368,16 @@ slope.k<-summary(monarch.regression)$coefficients[2,1]
 #predict k over time
 monarch1$k.linear<-intercept.k+slope.k*monarch1$year
 
-#now see what r looks like, using this fixed vector for k
-monarch1$r.linear.k<- (log(monarch1$Nt1/monarch1$Nt))/(1-(monarch1$Nt/k.linear))
+#now see what instantaneous r looks like, using this fixed vector for k
+monarch1$r.linear.k<- (log(monarch1$Nt1/monarch1$Nt))/(1-(monarch1$Nt/monarch1$k.linear))
 
-#hmm! Hmm! Interesting!! it's not really a linear decline in k, for sure, either.
+#hmm! Hmm! Interesting!! to be sure the evidence doesn't support this, let's look at the
+# fit by AICc using this linear declining k
+
+monarch.linear.decline.k<-nlsLM(Nt1~ Nt*exp(r*(1- Nt/k.linear)), 
+                                start=list(r=1.5), data=monarch1)
+summary(monarch.linear.decline.k)
+AIC(monarch.linear.decline.k)+AICcorrection(monarch1, 0)#add correction to get AICc
+
+#so this is interesting- the regime shift model with two breaks still fits best, but just assuming
+#a linear decline in k produced a nearly as good AIC. Not equivalent, but getting close.
