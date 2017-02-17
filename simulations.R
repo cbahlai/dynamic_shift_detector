0be008f0a430cf5c2899036424deffc19ff4be89
Christie Bahlai <cbahlai@msu.edu>
2017-02-17 15:20:26 -0500

    break it up into more discrete steps, test at each stage

--- simulations.R
+++ simulations.R
@@ -9,64 +9,100 @@
 #and while we're at it, simulations to test how this all works given different lengths of time series
 
 #create a function that will make fake data based on specified parameters
-#assume change, noise is given in percent (0-100) scale
+#assume change, noise is given in percent (0-100) scale, as is change to r, k
 
 fakedata<-function(startyear, Nyears, startPop, noise, startK, startR, breaks, changeK, changeR){
-  if(is.null(startyear)){ #set default values for all paremeters
+  if(missing(startyear)){ #set default values for all paremeters
     startyear<-1900
   }
-  if(is.null(Nyears)){
+  if(missing(Nyears)){
     Nyears<-20
   }
   #in order to set the default breaks, I need to know what the last year in the time range is
   #so let's create a vector of years going into the time series now
-  year<-startyear:(startyear+Nyears-1) #-1 because time range is inclusive of year 0
+  year<-seq(startyear, (startyear+Nyears-1)) #-1 because time range is inclusive of year 0
   lastyear<-max(year)-1 # max is second last year because we don't have Nt1 for last year
   
-  if(is.null(startPop)){#let's make popualtion size default to 1000
+  if(missing(startPop)){#let's make popultion size default to 1000
     startPop<-1000
   }
-  if(is.null(noise)){#no noise by default
+  if(missing(noise)){#no noise by default
     noise<-0
   }
-  if(is.null(startK)){ #so start population can grow by default
+  if(missing(startK)){ #so start population can grow by default
     startK<-1500
   }
-  if(is.null(startR)){
+  if(missing(startR)){
     startR<-1.5
   }
-  if(is.null(breaks)){
+  if(missing(breaks)){
     breaks<-list(lastyear)#no break model, break occurs at end of time series
   }
-  if(is.null(changeK)){ # by default, don't change K after a break
+  if(missing(changeK)){ # by default, don't change K after a break
     changeK<-0
   }
-  if(is.null(changeR)){ #same with r
+  if(missing(changeR)){ #same with r
     changeR<-0
   }
   
-  
   #create a vector for noise for each year- it will be random, in the range of % given
-  noisevector<-C()# make an empty vector
-  for (i in 1:years){
+  noisevector<-c()# make an empty vector
+  for (i in 1:length(year)){
     instant.buzz<-1+runif(1, -noise, noise)/100 #generate an instantaneous buzz :)
     noisevector<-c(noisevector, instant.buzz) #add that to the vector
   }
   
+  #create a vector of changes
+  
   #create a vector of changes to k
   k<-c(startK)# initiate vector with start value at k
-  
-  
-  #create a vector of changes to r
-  
-  #calculate Nt vector
-  Nt<-c(startpop) #create population vector with starting population as entry 1
-  for(i in 2:years){
-    Nt1<-Nt[i]*exp(r[i]*(1- Nt[i]/k[i]))*noisevector[i]
-    Nt<-c(Nt, Nt1)
-    
+  for (i in 1:length(year)){
+    if (year[i+1] %in% breaks){
+      nextk<-k[i]*sample(100-changeK, 100+changeK)/100 #randomly chose an increase or decrease in % change
+    } else{
+      nextk<-k[i] # or if it's not a break year, don't change k
+    }
+    k<-c(k, nextk)
   }
-  
+
+  # #create a vector of changes to r
+  # r<-c(startR)# initiate vector with start value at r
+  # for (i in 2:year){
+  #   if (year[i] %in% breaks){
+  #     nextr<-r[i-1]*sample(100-changeR, 100+changeR)/100 #randomly chose an increase or decrease in % change
+  #   } else{
+  #     nextr<-r[i-1] # or if it's not a break year, don't change k
+  #   }
+  #   r<-c(r, nextr)
+  # }
+  # 
+  # #calculate Nt vector
+  # Nt<-c(startpop) #create population vector with starting population as entry 1
+  # for(i in 2:year){
+  #   Nt1<-Nt[i]*exp(r[i]*(1- Nt[i]/k[i]))*noisevector[i]
+  #   Nt<-c(Nt, Nt1)
+  #   
+  # }
+  return(k)
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ricker.model<-nlsLM(Nt1~ Nt*exp(r*(1- Nt/k)), start=list(r=1.5, k=kest), data=data)
