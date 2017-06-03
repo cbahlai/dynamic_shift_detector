c934974fa26744b7ed6e47e32a504bc8b53a3555
Christie Bahlai <cbahlai@msu.edu>
2017-06-03 14:01:34 -0400

    get script tweaked for sim iterations 150-200

--- simulations.R
+++ simulations.R
@@ -486,7 +486,7 @@ test.iter<-iterate.breakitdown(startyear, Nyears=31, startPop, noise=5, startK=2
 simulation.results<-rbind(simulation.results, test.iter)
 
 test.iter<-iterate.breakitdown(startyear, Nyears=33, startPop, noise=5, startK=2000, startR=2, 
-                               changeK=40, changeR=20, nIter=1, numLoops)
+                               changeK=40, changeR=20, nIter=2, numLoops)
 #add these results to the data frame
 simulation.results<-rbind(simulation.results, test.iter)
 
@@ -495,5 +495,5 @@ proc.time() - ptm
 
 #save the simulation results in case we screw something up in the 
 #data manipulation stage
-write.csv(simulation.results, file="simulation.results2.csv")
+write.csv(simulation.results, file="simulation.results3.csv")
 
