280b73a21066b8ac14a305191513eddc74807328
Christie Bahlai <cbahlai@kent.edu>
2018-10-24 16:59:33 -0400

    update figures

--- plot_monarch_figures.R
+++ plot_monarch_figures.R
@@ -36,7 +36,7 @@ for (i in 1:(length(monarch$year))) {
   if(monarch$year[i]<2003.1){
     phase = c(phase, "A")
   }
-  else if (monarch$year[i]>2002.9& monarch$year[i]<2019){#dummy code if we need to add another break
+  else if (monarch$year[i]>2002.9& monarch$year[i]<2009){
     phase = c(phase, "B")
   }
   else {
@@ -51,10 +51,10 @@ for (i in 1:(length(monarch$year))) {
   if(monarch$year[i]<2002.1){
     phasea = c(phasea, "A")
   }
-  else if (monarch$year[i]>2002& monarch$year[i]<2019.1){#dummy code if we need to add another break
+  else if (monarch$year[i]>2002& monarch$year[i]<2009.1){
     phasea = c(phasea, "B")
   }
-  else {
+  else if(monarch$year[i]>2008.9){
     phasea = c(phasea, "C")
   }
 }
@@ -80,7 +80,9 @@ monarch.timeseries<-ggplot(monarch, aes(year, Nt, colour=phase, cex=1))+
   ylab("\nHectares occupied\n")+
   theme_bw()+
   coord_equal(ratio=0.45)+
-  geom_vline(xintercept=c(2003.5), colour="blue", linetype="longdash")+ 
+  geom_vline(xintercept=c(2003.5, 2008.5), colour="blue", linetype="longdash")+ 
+  geom_vline(xintercept=c(1998.5), colour="grey62", linetype="longdash")+
+  geom_vline(xintercept=c(1999.5, 2007.5), colour="grey62", linetype="dotted")+ 
   theme(legend.key = element_blank(), plot.margin=unit(c(15,0,0,0), "mm"))+
   theme(axis.text=axis.text.theme, axis.title=axis.title.theme, 
         legend.title=axis.title.theme, legend.text=axis.text.theme)+
@@ -100,8 +102,8 @@ monarch1<-addNt1(monarch)
 #
 ######################################
 phase.a<-function(x){x*exp(0.99*(1- x/10.11))}
-phase.b<-function(x){x*exp(0.84*(1- x/4.12))}
-
+phase.b<-function(x){x*exp(1.57*(1- x/5.65))}
+phase.c<-function(x){x*exp(1.16*(1- x/2.83))}
 
 monarch.ricker<-ggplot(monarch1, aes(Nt, Nt1, colour=phase, label=year))+
   scale_color_manual(values = pal)+
@@ -110,6 +112,7 @@ monarch.ricker<-ggplot(monarch1, aes(Nt, Nt1, colour=phase, label=year))+
   theme(legend.key = element_blank())+
   stat_function(fun=phase.a, colour=pal[1], size=1)+
   stat_function(fun=phase.b, colour=pal[2], size=1)+
+  stat_function(fun=phase.c, colour=pal[3], size=1)+
   geom_point(size=3)+
   geom_point(colour="black", pch=21, size=3)+
   coord_equal(ratio=1)+
