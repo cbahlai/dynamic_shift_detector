2c4c9b10a46867fa9d9a169613af43146989fdee
Christie Bahlai <cbahlai@kent.edu>
2020-09-17 14:35:11 -0400

    Update monarch_example.R
    add note to clarify year on data set

--- monarch_example.R
+++ monarch_example.R
@@ -3,7 +3,7 @@
 #bring in Monarch overwintering data
 
 monarch<-read.csv(file="C:/Users/cbahlai/Dropbox/Old_gigs/Zipkin/MonarchOW.csv", header=T)
-
+#note that data in this file labeled 1995 are from the winter 1994-1995 survey, 1996 is the 1995-1996 survey and so on
 
 #okay, let's see if it'll work
 source("dynamic_shift_detector.R")
